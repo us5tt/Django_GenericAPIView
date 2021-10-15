26859b102b73905d20211653909b036404717482
us5tt <serg.manko80@gmail.com>
2021-10-15 22:38:05 +0300

    add templates, home page

--- django_test/api/views.py
+++ django_test/api/views.py
@@ -1,14 +1,13 @@
+from django.template.response import TemplateResponse
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
-
-from django.http import HttpResponse
 
 from .models import Pars
 from .serializers import ParsSerializer
 
 
 def index(request):
-    return HttpResponse("main page coming soon")
+    return TemplateResponse (request, "api/index.html")
 
 
 class ParsView(ListCreateAPIView):
